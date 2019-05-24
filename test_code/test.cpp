26acb56da5cc90ac32ca931f74de085590f07567
Savindra <savindran1989@gmail.com>
2019-05-25 00:11:13 +0530

    Test code repository change

---
+++ test_code/test.cpp
@@ -0,0 +1,28 @@
+#include "CppUTest/TestHarness.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+extern "C"
+{
+
+}
+
+
+TEST_GROUP(Test)
+{	
+    void setup()
+    {	
+
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+TEST(Test, first_test)
+{
+	CHECK_TRUE(true);
+}

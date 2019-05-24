5ba7f896ea62601273bbf73c99e1e34afb4a0126
Savindra Kumar <45175425+SavindraKumar@users.noreply.github.com>
2019-05-24 10:55:50 -0500

    Add test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+    bool bIsConditionOk = false;
+    
+    if (bIsConditionOk)
+    {
+        printf("Condition Ok");
+    }
+    else
+    {
+        printf("Condition Not Ok");
+    }
+}//end main
+   
